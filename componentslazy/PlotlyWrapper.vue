2a2dfbb4e229d6062868757edf501c9a0447fb67
Declan Murphy <deccy06@gmail.com>
2023-07-18 12:28:15 +0200

    Add plotly & add graph to single view circuit page

---
+++ componentslazy/PlotlyWrapper.vue
@@ -0,0 +1,27 @@
+<script>
+import { Plotly } from 'vue-plotly'
+
+export default {
+  components: {
+    Plotly,
+  },
+
+  props: {
+    dataPoints: {
+      type: Array,
+      required: true,
+    },
+    layout: {
+      title: 'My graph',
+    },
+  },
+}
+</script>
+
+<template>
+  <Plotly
+    :data="dataPoints"
+    :layout="layout"
+    :display-mode-bar="false"
+  ></Plotly>
+</template>
--- pages/circuit/_id.vue
+++ pages/circuit/_id.vue
@@ -2,19 +2,42 @@
 import Vue from 'vue'
 export default Vue.extend({
   async asyncData({ $axios, params }) {
-    const { id } = params
-    const { circuit, payload } = await $axios.$get(
-      'http://localhost:3333/circuits/' + id
-    )
-    return { circuit, dataPoints: [payload] }
+    try {
+      const { id } = params
+      const { circuit, payload } = await $axios.$get(
+        'http://localhost:3333/circuits/' + id
+      )
+
+      payload.data.type = 'scatter'
+      return { circuit, dataPoints: [payload?.data] }
+    } catch (e: any) {
+      return { error: e }
+    }
+  },
+  components: {
+    PlotlyWrapper: () => import('./../../componentslazy/PlotlyWrapper.vue'),
   },
 })
 </script>
 
 <template>
   <div class="flex flex-col gap-3 w-full justify-center">
-    <div class="flex text-xl justify-center">
-      <span class="font-bold">Circuit:&nbsp;</span> {{ circuit.name }}
+    <div v-if="dataPoints">
+      <div class="flex text-xl justify-center">
+        <span class="font-bold">Circuit:&nbsp;</span> {{ circuit.name }}
+      </div>
+      <client-only>
+        <PlotlyWrapper
+          class="min-h-[500px] max-h-[calc(100vh-150px)]"
+          v-bind="{ dataPoints }"
+        />
+      </client-only>
+    </div>
+    <div class="flex text-xl justify-center" v-if="error">
+      <p class="text-red-500 text-2xl">
+        Error generating graph
+        {{ error?.message ? ` - ${error?.message}` : '' }}
+      </p>
     </div>
   </div>
 </template>
